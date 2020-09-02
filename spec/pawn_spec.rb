5c36b7adcb8ec20ab1efbbffc772d77c0e47639b
Raymond Oh <raymond.oh@gmail.com>
2020-09-02 17:36:29 -0400

    add and incorporate test 5 - Pawn can capture en passant

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -6,7 +6,7 @@ grid1 = []
   grid1.push(row)
 end
 
-describe 'Pawn - moves' do
+describe 'Pawn - initial moves' do
   let(:wp1) { Pawn.new('white', 'player1') }
   let(:bp1) { Pawn.new('black', 'player2') }
   let(:board) { double }
@@ -20,13 +20,23 @@ describe 'Pawn - moves' do
     allow(board).to receive(:grid).and_return(grid1)
     wp1.history.push('A2')
     expect(wp1.possible_moves(board)).to include('A3', 'A4')
-    expect(wp1.possible_moves(board)).to_not include('B3')
+    expect(wp1.possible_moves(board).length).to eql(2)
+  end
+
+  it 'can move forward only one space after first move' do
+    allow(board).to receive(:grid).and_return(grid1)
     bp1.history.push('B7')
     expect(bp1.possible_moves(board)).to include('B6', 'B5')
     bp1.history.push('B6')
     expect(bp1.possible_moves(board)).to include('B5')
     expect(bp1.possible_moves(board).length).to eql(1)
   end
+end
+
+describe 'Pawn - blocked paths' do
+  let(:wp1) { Pawn.new('white', 'player1') }
+  let(:bp1) { Pawn.new('black', 'player2') }
+  let(:board) { double }
 
   it 'can not single move if piece is in front of it' do
     grid1[2][0] = bp1
@@ -60,4 +70,16 @@ describe 'Pawn - capturing moves' do
     expect(wp1.possible_moves(board)).to include('B5', 'C5')
   end
 
+  it 'can capture en passant' do
+    wp1.history = %w[C2 C4]
+    bp1.history = %w[B7 B5 B4]
+    wp1.ep_flag = true
+    allow(bp1).to receive(:square_occupied?).with('C4', board).and_return(wp1)
+    allow(bp1).to receive(:square_occupied?).with('A4', board).and_return(false)
+    allow(bp1).to receive(:square_occupied?).with('A3', board).and_return(false)
+    allow(bp1).to receive(:square_occupied?).with('B3', board).and_return(false)
+    allow(bp1).to receive(:square_occupied?).with('C3', board).and_return(false)
+    expect(bp1.possible_moves(board).length).to eql(2)
+    expect(bp1.possible_moves(board)).to include('B3', 'C3')
+  end
 end
