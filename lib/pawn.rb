5f07434a5e056fc18052cf5ca3f38328360a9851
Raymond Oh <raymond.oh@gmail.com>
2020-09-02 13:23:53 -0400

    Pawn Class passes 3/3 tests

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -2,4 +2,64 @@ require_relative 'piece'
 
 # pawn piece class
 class Pawn < Piece
+  attr_reader :symbol
+  def initialize(color, player_id)
+    super(color, player_id)
+    @symbol = @color == 'white' ? "\u2659" : "\u265F"
+  end
+
+  # black pawns move 'N' to 'S', white pawns move 'S' to 'N'
+  def possible_moves(board)
+    poss_move_array = []
+    current_square = @history.last
+    current_coords = parse_coord(current_square)
+
+    poss_move_array.push(single_march(current_coords)) if single_march(current_coords)
+    poss_move_array.push(double_march(current_coords)) if first_move?
+    valid_diagonals(current_coords, board).each { |move| poss_move_array.push(move) }
+
+    poss_move_array
+  end
+
+  private
+
+  # translate 'A2' type move from [row][col] coords
+  def parse_move(coords)
+    row = coords[0] + 1
+    col = (coords[1] + 65).chr
+    col + row.to_s
+  end
+
+  def single_march(coords)
+    march = @color == 'white' ? [coords[0] + 1, coords[1]] : [coords[0] - 1, coords[1]]
+    on_board?(parse_move(march)) ? parse_move(march) : false
+  end
+
+  def double_march(coords)
+    march = @color == 'white' ? [coords[0] + 2, coords[1]] : [coords[0] - 2, coords[1]]
+    parse_move(march)
+  end
+
+  def get_diagonals(coords)
+    if @color == 'white'
+      diag1 = [coords[0] + 1, coords[1] - 1]
+      diag2 = [coords[0] + 1, coords[1] + 1]
+    else # 'black'
+      diag1 = [coords[0] - 1, coords[1] - 1]
+      diag2 = [coords[0] - 1, coords[1] + 1]
+    end
+    [diag1, diag2]
+  end
+
+  def valid_diagonals(coords, board)
+    diag1 = parse_move(get_diagonals(coords)[0])
+    diag2 = parse_move(get_diagonals(coords)[1])
+    piece1 = square_occupied?(diag1, board)
+    piece2 = square_occupied?(diag2, board)
+    valid_diagonals = []
+    valid_diagonals.push(diag1) if piece1 && !my_piece?(piece1)
+    valid_diagonals.push(diag2) if piece2 && !my_piece?(piece2)
+    valid_diagonals
+  end
+
 end
--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -1,9 +1,35 @@
 require './lib/pawn'
 
+grid1 = Array.new(8) { Array.new(8) { nil } }
+
 describe Pawn do
-  xit 'has an appropriate pawn unicode symbol'
+  let(:wp1) { described_class.new('white', 'player1') }
+  let(:bp1) { described_class.new('black', 'player2') }
+  let(:board) { double }
 
-  xit 'can move forward one or two spaces to start'
+  it 'has an appropriate pawn unicode symbol' do
+    expect(wp1.symbol).to eql("\u2659")
+    expect(bp1.symbol).to eql("\u265F")
+  end
 
-  xit 'can "move"(capture) if opponent piece is diagonal'
+  it 'can move forward one or two spaces to start' do
+    allow(board).to receive(:grid).and_return(grid1)
+    wp1.history.push('A2')
+    expect(wp1.possible_moves(board)).to include('A3', 'A4')
+    expect(wp1.possible_moves(board)).to_not include('B3')
+    bp1.history.push('B7')
+    expect(bp1.possible_moves(board)).to include('B6', 'B5')
+    bp1.history.push('B6')
+    expect(bp1.possible_moves(board)).to include('B5')
+    expect(bp1.possible_moves(board).length).to eql(1)
+  end
+
+  it 'can "move"(capture) if opponent piece is diagonal' do
+    wp1.history.push('B2')
+    wp1.history.push('B4')
+    allow(wp1).to receive(:square_occupied?).with('C5', board).and_return(bp1)
+    allow(wp1).to receive(:square_occupied?).with('A5', board).and_return(false)
+    expect(wp1.possible_moves(board).length).to eql(2)
+    expect(wp1.possible_moves(board)).to include('B5', 'C5')
+  end
 end
