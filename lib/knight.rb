87dd0d97f5c664f5caa42f82b044e8dff7178de1
Raymond Oh <raymond.oh@gmail.com>
2020-09-02 18:01:48 -0400

    Knight class passes 1/3 tests - Knight.symbol

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,3 +1,12 @@
+require_relative './piece'
+
 # knight piece class
-class Knight < piece
+class Knight < Piece
+  attr_reader :symbol
+  def initialize(color, player_id)
+    super(color, player_id)
+    @symbol = @color == 'white' ? "\u2658" : "\u265E"
+    @history = []
+  end
+
 end
--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -1,7 +1,15 @@
 require './lib/knight'
 
 describe Knight do
-  xit 'has unique/appropriate unicode symbols'
-  xit 'moves like a knight piece'
+  let(:bk1) { Knight.new('black', 'player2') }
+  let(:board) { double }
+
+  it 'has unique/appropriate unicode symbols' do
+    expect(bk1.symbol).to eql("\u265E")
+  end
+
+  xit 'moves like a knight piece' do
+  end
+
   xit 'only has possible moves that stay on board'
 end
