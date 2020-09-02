db0bdbcad35123f1f33bd8763987a06a03000f53
Raymond Oh <raymond.oh@gmail.com>
2020-09-02 10:39:50 -0400

    establish files and tests for Pawn Class

---
+++ lib/pawn.rb
@@ -0,0 +1,5 @@
+require_relative 'piece'
+
+# pawn piece class
+class Pawn < Piece
+end
---
+++ spec/pawn_spec.rb
@@ -0,0 +1,9 @@
+require './lib/pawn'
+
+describe Pawn do
+  xit 'has an appropriate pawn unicode symbol'
+
+  xit 'can move forward one or two spaces to start'
+
+  xit 'can "move"(capture) if opponent piece is diagonal'
+end
