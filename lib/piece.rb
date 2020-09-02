83531e61f384e352f940c6df370fe47775373ae7
Raymond Oh <raymond.oh@gmail.com>
2020-09-02 09:06:35 -0400

    test and setup Piece class

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,4 +1,39 @@
 # Generic Chess Piece Class
 class Piece
+  attr_accessor :color, :player_id, :history, :opponent
+  def initialize(color, player_id)
+    @color = color
+    @player_id = player_id
+    @history = []
+    @opponent = @color == 'white' ? 'black ' : 'white'
+  end
 
+  private
+
+  # methods to help define general movement logic
+  def parse_coord(square)
+    square = square.split('')
+    row = square[1].to_i - 1
+    col = square[0].bytes[0] - 65
+    [row, col]
+  end
+
+  def first_move?
+    @history.length < 2
+  end
+
+  def on_board?(square)
+    coord = parse_coord(square)
+    coord[0].between?(0, 7) && coord[1].between?(0, 7)
+  end
+
+  def square_occupied?(square, board)
+    coord = parse_coord(square)
+    space = board.grid[coord[0]][coord[1]]
+    space.nil? ? false : space
+  end
+
+  def my_piece?(piece)
+    piece.color == @color
+  end
 end
--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -1,5 +1,18 @@
 require './lib/piece'
 
 describe Piece do
-  
+  let(:generic) { described_class.new('black', 'player1') }
+  it 'has a color id' do
+    expect(generic.color).to eql('black')
+  end
+  it 'id\'s the opponent color' do
+    expect(generic.opponent).to eql('white')
+  end
+  it 'has a player id' do
+    expect(generic.player_id).to eql('player1')
+  end
+  it 'has a move history' do
+    expect(generic.history).to be_an(Array)
+    expect(generic.history.length).to eql(0)
+  end
 end
