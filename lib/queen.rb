b8ccb5e1d1fbd7831e20fc94fbd8746224ebf2ff
Raymond Oh <raymond.oh@gmail.com>
2020-09-03 14:05:32 -0400

    established files and rspec tests for Queen class

---
+++ lib/queen.rb
@@ -0,0 +1,6 @@
+require_relative './piece'
+
+# Queen piece class
+class Queen < Piece
+
+end
---
+++ spec/queen_spec.rb
@@ -0,0 +1,8 @@
+require './lib/queen'
+
+describe Queen do
+  xit 'has unique/appropriate unicode symbols'
+  xit 'moves like a queen piece (diag/horiz/vert)'
+  xit 'can move to spot occupied by opponent piece (but not beyond)'
+  xit 'can not move if surrounded by its own pieces'
+end
