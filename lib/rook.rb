d1f39e962dcd97a4fba6d9ee586a735f863b8837
Raymond Oh <raymond.oh@gmail.com>
2020-09-03 12:28:25 -0400

    pass Rook class test 1/4 - Rook.symbol

--- lib/rook.rb
+++ lib/rook.rb
@@ -1,6 +1,11 @@
-require_relative '/.piece'
+require_relative './piece'
 
 # Rook piece class
-class Rook < piece
-
+class Rook < Piece
+  attr_reader :symbol
+  def initialize(color, player_id)
+    super(color, player_id)
+    @symbol = @color == 'white' ? "\u2656" : "\u265C"
+    @history = []
+  end
 end
--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -1,7 +1,15 @@
 require './lib/rook'
 
 describe Rook do
-  xit 'has unique/appropriate unicode symbols'
+  let(:wr1) { Rook.new('white', 'player1') }
+  let(:br1) { Rook.new('black', 'player1') }
+  let(:board) { double }
+
+  it 'has unique/appropriate unicode symbols' do
+    expect(wr1.symbol).to eql("\u2656")
+    expect(br1.symbol).to eql("\u265C")
+  end
+
   xit 'moves like a rook piece (horiz/vert)'
   xit 'can move to spot occupied by opponent piece (but not beyond)'
   xit 'can not move if surrounded by its own pieces'
