f094e072d30bb6b9ae5a2eea1497ce255316c244
Raymond Oh <raymond.oh@gmail.com>
2020-09-03 11:11:42 -0400

    pass Bishop class test 4/4 - surrounded Bishop.possible_moves

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -1,9 +1,10 @@
 require './lib/bishop'
 
 describe Bishop do
-  let(:wb1) { Bishop.new('white', 'player1') } 
+  let(:wb1) { Bishop.new('white', 'player1') }
   let(:bb1) { Bishop.new('black', 'player2') }
   let(:board) { double }
+  before(:each) { wb1.history.push('D4') }
 
   it 'has unique/appropriate unicode symbols' do
     expect(wb1.symbol).to eql("\u2657")
@@ -11,7 +12,6 @@ describe Bishop do
   end
 
   it 'moves like a bishop piece (diagonally)' do
-    wb1.history.push('D4')
     allow(wb1).to receive(:square_occupied?).and_return(false)
 
     expect(wb1.possible_moves(board).length).to eql(13)
@@ -19,7 +19,6 @@ describe Bishop do
   end
 
   it 'can not move to a spot beyond where its path is blocked' do
-    wb1.history.push('D4')
     allow(wb1).to receive(:square_occupied?).and_return(false)
     allow(wb1).to receive(:square_occupied?).with('F6', board).and_return(bb1)
 
@@ -28,5 +27,9 @@ describe Bishop do
     expect(wb1.possible_moves(board)).to_not include('G7', 'H8')
   end
 
-  xit 'can not move if surrounded by other pieces'
+  it 'can not move if surrounded by other pieces' do
+    allow(wb1).to receive(:square_occupied?).and_return('yes')
+    
+    expect(wb1.possible_moves(board).length).to be_zero
+  end
 end
