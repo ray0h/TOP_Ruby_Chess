209f3c2c03ffede9327acfd894184958a613f06f
Raymond Oh <raymond.oh@gmail.com>
2020-09-03 14:23:42 -0400

    pass Queen class test 2/4 - Queen.possible_moves

--- lib/queen.rb
+++ lib/queen.rb
@@ -8,4 +8,59 @@ class Queen < Piece
     @symbol = @color == 'white' ? "\u2655" : "\u265B"
     @history = []
   end
+
+  def possible_moves(board)
+    current_square = @history.last
+    current_coords = parse_coord(current_square)
+    queen_moves(current_coords, board)
+  end
+
+  private
+
+  def parse_square(coord)
+    row = (coord[0] + 1).to_s
+    col = (coord[1] + 65).chr
+    col + row
+  end
+
+  def possible_diagonal(coord, row_coef, col_coef, board)
+    inc = 1
+    diagonals = []
+    loop do
+      next_square = parse_square([coord[0] + (inc * row_coef), coord[1] + (inc * col_coef)])
+
+      diagonals.push(next_square) if on_board?(next_square)
+      inc += 1
+      break if !on_board?(next_square)
+    end
+    diagonals
+  end
+
+  def possible_line(coord, row_coef, col_coef, board)
+    inc = 1
+    line = []
+    loop do
+      next_square = parse_square([coord[0] + (inc * row_coef), coord[1] + (inc * col_coef)])
+     
+      line.push(next_square) if on_board?(next_square)
+      inc += 1
+      break if !on_board?(next_square)
+    end
+    line
+  end
+
+  def queen_moves(coord, board)
+    n = possible_line(coord, 1, 0, board)
+    e = possible_line(coord, 0, 1, board)
+    w = possible_line(coord, 0, -1, board)
+    s = possible_line(coord, -1, 0, board)
+
+    nw = possible_diagonal(coord, 1, -1, board)
+    ne = possible_diagonal(coord, 1, 1, board)
+    sw = possible_diagonal(coord, -1, -1, board)
+    se = possible_diagonal(coord, -1, 1, board)
+
+    possible_moves = n + e + w + s + nw + ne + sw + se
+    possible_moves
+  end
 end
--- spec/queen_spec.rb
+++ spec/queen_spec.rb
@@ -4,13 +4,21 @@ describe Queen do
   let(:wqn) { Queen.new('white', 'player1') }
   let(:bqn) { Queen.new('black', 'player2') }
   let(:board) { double }
+  before(:each) do
+    wqn.history.push('D4')
+    allow(wqn).to receive(:square_occupied?).and_return(false)
+  end
 
   it 'has unique/appropriate unicode symbols' do
     expect(wqn.symbol).to eql("\u2655")
     expect(bqn.symbol).to eql("\u265B")
   end
 
-  xit 'moves like a queen piece (diag/horiz/vert)'
+  it 'moves like a queen piece (diag/horiz/vert)' do
+    expect(wqn.possible_moves(board).length).to eql(27)
+    expect(wqn.possible_moves(board)).to include('A1', 'A4', 'A7', 'D1', 'D8', 'G1', 'H4', 'H8')
+  end
+
   xit 'can move to spot occupied by opponent piece (but not beyond)'
   xit 'can not move if surrounded by its own pieces'
 end
