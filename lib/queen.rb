7e90ce0151a9c646452d45dd425c6ad4add3f435
Raymond Oh <raymond.oh@gmail.com>
2020-09-03 14:11:51 -0400

    pass Queen class test 1/4 - Rook.symbol

--- lib/queen.rb
+++ lib/queen.rb
@@ -2,5 +2,10 @@ require_relative './piece'
 
 # Queen piece class
 class Queen < Piece
-
+  attr_reader :symbol
+  def initialize(color, player_id)
+    super(color, player_id)
+    @symbol = @color == 'white' ? "\u2655" : "\u265B"
+    @history = []
+  end
 end
--- spec/queen_spec.rb
+++ spec/queen_spec.rb
@@ -1,7 +1,15 @@
 require './lib/queen'
 
 describe Queen do
-  xit 'has unique/appropriate unicode symbols'
+  let(:wqn) { Queen.new('white', 'player1') }
+  let(:bqn) { Queen.new('black', 'player2') }
+  let(:board) { double }
+
+  it 'has unique/appropriate unicode symbols' do
+    expect(wqn.symbol).to eql("\u2655")
+    expect(bqn.symbol).to eql("\u265B")
+  end
+
   xit 'moves like a queen piece (diag/horiz/vert)'
   xit 'can move to spot occupied by opponent piece (but not beyond)'
   xit 'can not move if surrounded by its own pieces'
