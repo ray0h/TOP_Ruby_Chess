f766b99217d927a3afc664f29948a1bc077e0f3f
Raymond Oh <raymond.oh@gmail.com>
2020-09-03 09:46:24 -0400

    establish files and rspec tests for Bishop class

---
+++ lib/bishop.rb
@@ -0,0 +1,6 @@
+require_relative './piece'
+
+# Bishop piece class
+class Bishop < Piece
+
+end
---
+++ spec/bishop_spec.rb
@@ -0,0 +1,8 @@
+require './lib/bishop'
+
+describe bishop do
+  xit 'has unique/appropriate unicode symbols'
+  xit 'moves like a bishop piece (diagonally)'
+  xit 'can not move to a spot beyond where its path is blocked'
+  xit 'can not move if surrounded by other pieces'
+end
