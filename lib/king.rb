2ae16354425f3da04a33c4b261b2a313d5bfc289
Raymond Oh <raymond.oh@gmail.com>
2020-09-03 15:07:18 -0400

    pass King class test 1/6 - King.symbol

--- lib/king.rb
+++ lib/king.rb
@@ -2,5 +2,10 @@ require_relative './piece'
 
 # King piece class
 class King < Piece
-
+  attr_reader :symbol
+  def initialize(color, player_id)
+    super(color, player_id)
+    @symbol = @color == 'white' ? "\u2654" : "\u265A"
+    @history = []
+  end
 end
--- spec/king_spec.rb
+++ spec/king_spec.rb
@@ -1,13 +1,25 @@
 require './lib/king'
 
 describe 'King - symbols and basic moves' do
-  xit 'has unique/appropriate unicode symbols'
+  let(:wkg) { King.new('white', 'player1') }
+  let(:bkg) { King.new('black', 'player2') }
+  let(:board) { double }
+  before(:each) do
+    wkg.history.push('D4')
+    allow(wkg).to receive(:square_occupied?).and_return(false)
+  end
+
+  it 'has unique/appropriate unicode symbols' do
+    expect(wkg.symbol).to eql("\u2654")
+    expect(bkg.symbol).to eql("\u265A")
+  end
+
   xit 'moves like a king piece - 1 space, diag/horiz/vert'
   xit 'can move to spot occupied by opponent piece'
   xit 'can not move if surrounded by its own pieces'
 end
 
-describe 'King - moves unique to king class'
+describe 'King - moves unique to king class' do
   xit 'can not move into an empty square within opponent piece\'s possible moves'
   xit 'can access appropriate squares if castling'
 end
