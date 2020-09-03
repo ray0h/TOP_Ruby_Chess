51c7fc080b40a8ed4814b6643aacae28995489d4
Raymond Oh <raymond.oh@gmail.com>
2020-09-02 20:01:08 -0400

    Knight class passes 2/3 and 3/3 tests - Knight.possible_moves

--- lib/knight.rb
+++ lib/knight.rb
@@ -9,4 +9,31 @@ class Knight < Piece
     @history = []
   end
 
+  def possible_moves
+    current_square = @history.last
+    current_coords = parse_coord(current_square)
+
+    knight_moves(current_coords)
+  end
+
+  private
+
+  def parse_move(coord)
+    row = (coord[0] + 1).to_s
+    col = (coord[1] + 65).chr
+    col + row
+  end
+
+  def knight_moves(coords)
+    poss_moves = []
+    (-2..2).each do |i|
+      (-2..2).each do |j|
+        next if i.abs == j.abs || i.zero? || j.zero?
+
+        move = parse_move([(coords[0] + i), (coords[1] + j)])
+        poss_moves.push(move) if on_board?(move)
+      end
+    end
+    poss_moves
+  end
 end
--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -8,8 +8,15 @@ describe Knight do
     expect(bk1.symbol).to eql("\u265E")
   end
 
-  xit 'moves like a knight piece' do
+  it 'moves like a knight piece' do
+    bk1.history.push('D4')
+    expect(bk1.possible_moves.length).to eql(8)
+    expect(bk1.possible_moves).to include('B3', 'B5', 'C2', 'C6', 'E2', 'E6', 'F3', 'F5')
   end
 
-  xit 'only has possible moves that stay on board'
+  it 'only has possible moves that stay on board' do
+    bk1.history.push('G1')
+    expect(bk1.possible_moves.length).to eql(3)
+    expect(bk1.possible_moves).to include('E2', 'F3', 'H3')
+  end
 end
