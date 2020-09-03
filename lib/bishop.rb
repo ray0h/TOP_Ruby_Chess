6da08588f778fed8508e7b621d533d12bd660a49
Raymond Oh <raymond.oh@gmail.com>
2020-09-03 09:56:19 -0400

    pass Bishop class test 1/4 - Bishop.symbol

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -2,5 +2,10 @@ require_relative './piece'
 
 # Bishop piece class
 class Bishop < Piece
-
+  attr_reader :symbol
+  def initialize(color, player_id)
+    super(color, player_id)
+    @symbol = @color == 'white' ? "\u2657" : "\u265D"
+    @history = []
+  end
 end
--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -1,8 +1,16 @@
 require './lib/bishop'
 
-describe bishop do
-  xit 'has unique/appropriate unicode symbols'
-  xit 'moves like a bishop piece (diagonally)'
+describe Bishop do
+  let(:wb1) { Bishop.new('white', 'player1') } 
+  let(:bb1) { Bishop.new('black', 'player2') }
+  
+  it 'has unique/appropriate unicode symbols' do
+    expect(wb1.symbol).to eql("\u2657")
+    expect(bb1.symbol).to eql("\u265D")
+  end
+
+  xit 'moves like a bishop piece (diagonally)' do
+  end
   xit 'can not move to a spot beyond where its path is blocked'
   xit 'can not move if surrounded by other pieces'
 end
