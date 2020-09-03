3d949536c7f333a5d28aaf95101c766fedd253fa
Raymond Oh <raymond.oh@gmail.com>
2020-09-03 15:00:25 -0400

    establish files and rspect tests for King class

---
+++ lib/king.rb
@@ -0,0 +1,6 @@
+require_relative './piece'
+
+# King piece class
+class King < Piece
+
+end
---
+++ spec/king_spec.rb
@@ -0,0 +1,13 @@
+require './lib/king'
+
+describe 'King - symbols and basic moves' do
+  xit 'has unique/appropriate unicode symbols'
+  xit 'moves like a king piece - 1 space, diag/horiz/vert'
+  xit 'can move to spot occupied by opponent piece'
+  xit 'can not move if surrounded by its own pieces'
+end
+
+describe 'King - moves unique to king class'
+  xit 'can not move into an empty square within opponent piece\'s possible moves'
+  xit 'can access appropriate squares if castling'
+end
