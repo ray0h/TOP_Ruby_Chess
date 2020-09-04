7600e3c0451c0f28047596b76c5bb52f6e3a9dad
Raymond Oh <raymond.oh@gmail.com>
2020-09-04 12:38:25 -0400

    add new test, pass Player class test 3/11 - Player.start_move, rej if not correct sq length

--- lib/player.rb
+++ lib/player.rb
@@ -13,9 +13,36 @@ class Player
 
   def start_move
     print "#{id}, pick a square: "
+    init_square = gets.to_s.chomp
+    move = valid_square?(init_square)
+
+    until move['valid']
+      print "#{move['error_msg']}\n"
+      print "#{id}, pick a square: "
+      init_square = gets.to_s.chomp
+      move = valid_square?(init_square)
+    end
+    init_square
   end
 
   private
 
   attr_writer :id
+
+  def parse_coord(square)
+    coord = square.split('')
+    row = coord[0].to_i + 1
+    col = coord[1].bytes[0] - 65
+    [row, col]
+  end
+
+  def valid_id(square)
+    square.length == 2
+  end
+
+  def valid_square?(square)
+    return { 'valid' => false, 'error_msg' => 'Enter a valid square' } unless valid_id(square)
+
+    { 'valid' => true, 'error_msg' => '' }
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,18 +4,24 @@ describe 'Player - id' do
   let(:player1) { Player.new('player1') }
   it 'can change name/id' do
     expect(player1.id).to eql('player1')
-    # allow(player1).to receive(:change_id)
     allow(player1).to receive(:gets).and_return('Bob')
     expect { player1.change_id }.to output('player1, what should I call you now? ').to_stdout
     expect(player1.id).to eql('Bob')
   end
 end
 
-describe 'Player - moves - picking intitial square' do
+describe 'Player - moves - picking initial square' do
   let(:player1) { Player.new('player1') }
-
   it 'asks for an initial square' do
+    allow(player1).to receive(:gets).and_return('B2')
     expect { player1.start_move }.to output('player1, pick a square: ').to_stdout
+  end
+
+  it 'rejects if not a valid square length' do
+    allow(player1).to receive(:gets).and_return('Q', 'B15', 'A1')
+    string = 'player1, pick a square: '
+    2.times { string += "Enter a valid square\nplayer1, pick a square: " }
+    expect { player1.start_move }.to output(string).to_stdout
   end
 
   xit 'rejects if initial square is not on board'
