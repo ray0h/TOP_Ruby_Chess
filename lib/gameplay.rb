1fa160625b193326fb8fca978bbb75e14c60f1af
Raymond Oh <raymond.oh@gmail.com>
2020-09-05 16:20:57 -0400

    pass Gameplay class tests 1/10, 2/10, creating players, chessboard

--- lib/gameplay.rb
+++ lib/gameplay.rb
@@ -8,6 +8,19 @@ require_relative './rook'
 require_relative './queen'
 require_relative './king'
 
+# Gameplay class
 class Gameplay
+  attr_reader :player1, :player2, :board
+  def initialize
+    @player1, @player2 = create_players
+    @board = Board.new
+  end
 
+  private
+
+  def create_players
+    player1 = Player.new('player1')
+    player2 = Player.new('player2')
+    [player1, player2]
+  end
 end
--- spec/gameplay_spec.rb
+++ spec/gameplay_spec.rb
@@ -1,9 +1,20 @@
 require './lib/gameplay'
 
 describe Gameplay do
+  let(:game) { Gameplay.new }
+
   context 'setting up' do
+    it 'sets up two players' do
+      expect(game.player1.class.name).to eql('Player')
+      expect(game.player2.class.name).to eql('Player')
+    end
+
+    it 'sets up a chessboard' do
+      expect(game.board.class.name).to eql('Board')
+    end
+
     xit 'assigns a set of pieces to a player'
-    xit 'sets up the chessboard for a new game'
+    xit 'adds pieces to the board for a new game'
   end
 
   context 'basic gameplay' do
