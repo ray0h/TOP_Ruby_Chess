3e15ba6151bc3af0eb305538329f92657901d847
Raymond Oh <raymond.oh@gmail.com>
2020-09-05 16:10:28 -0400

    establish files and rspec tests for Gameplay class

---
+++ lib/gameplay.rb
@@ -0,0 +1,13 @@
+require_relative './player'
+require_relative './board'
+require_relative './piece'
+require_relative './pawn'
+require_relative './knight'
+require_relative './bishop'
+require_relative './rook'
+require_relative './queen'
+require_relative './king'
+
+class Gameplay
+
+end
---
+++ spec/gameplay_spec.rb
@@ -0,0 +1,20 @@
+require './lib/gameplay'
+
+describe Gameplay do
+  context 'setting up' do
+    xit 'assigns a set of pieces to a player'
+    xit 'sets up the chessboard for a new game'
+  end
+
+  context 'basic gameplay' do
+    xit 'removes captured pieces'
+    xit 'recognizes checks'
+    xit 'recognizes checkmates'
+    xit 'recognizes stalemates'
+  end
+
+  context 'advanced gameplay' do
+    xit 'handles castling moves'
+    xit 'handles pawn promotion'
+  end
+end
