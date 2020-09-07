498472b5f500df7f3ee68e83813411c06cca0dc3
Raymond Oh <raymond.oh@gmail.com>
2020-09-07 14:15:43 -0400

    add and pass Knight class test 4/4 - can not move to space occupied by its team piece

--- lib/knight.rb
+++ lib/knight.rb
@@ -9,23 +9,37 @@ class Knight < Piece
     @history = []
   end
 
-  def possible_moves
+  def possible_moves(board)
     current_square = @history.last
     current_coords = parse_coord(current_square)
 
-    knight_moves(current_coords)
+    knight_moves(current_coords, board)
   end
 
   private
 
-  def knight_moves(coords)
+  def valid_empty_square?(piece, square)
+    !piece && on_board?(square)
+  end
+
+  def opponent_piece?(piece)
+    piece && !my_piece?(piece)
+  end
+
+  def eval_move(coords, row_inc, col_inc, board)
+    move = parse_square([(coords[0] + row_inc), (coords[1] + col_inc)])
+    piece = square_occupied?(move, board)
+    opponent_piece?(piece) || valid_empty_square?(piece, move) ? move : false
+  end
+
+  def knight_moves(coords, board)
     poss_moves = []
     (-2..2).each do |i|
       (-2..2).each do |j|
         next if i.abs == j.abs || i.zero? || j.zero?
 
-        move = parse_square([(coords[0] + i), (coords[1] + j)])
-        poss_moves.push(move) if on_board?(move)
+        move = eval_move(coords, i, j, board)
+        poss_moves.push(move) if move
       end
     end
     poss_moves
--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -2,7 +2,9 @@ require './lib/knight'
 
 describe Knight do
   let(:bk1) { Knight.new('black', 'player2') }
+  let(:bk2) { Knight.new('black', 'player2') }
   let(:board) { double }
+  before(:each) { allow(bk1).to receive(:square_occupied?).and_return(false) }
 
   it 'has unique/appropriate unicode symbols' do
     expect(bk1.symbol).to eql("\u265E")
@@ -10,13 +12,20 @@ describe Knight do
 
   it 'moves like a knight piece' do
     bk1.history.push('D4')
-    expect(bk1.possible_moves.length).to eql(8)
-    expect(bk1.possible_moves).to include('B3', 'B5', 'C2', 'C6', 'E2', 'E6', 'F3', 'F5')
+    expect(bk1.possible_moves(board).length).to eql(8)
+    expect(bk1.possible_moves(board)).to include('B3', 'B5', 'C2', 'C6', 'E2', 'E6', 'F3', 'F5')
   end
 
   it 'only has possible moves that stay on board' do
     bk1.history.push('G1')
-    expect(bk1.possible_moves.length).to eql(3)
-    expect(bk1.possible_moves).to include('E2', 'F3', 'H3')
+    expect(bk1.possible_moves(board).length).to eql(3)
+    expect(bk1.possible_moves(board)).to include('E2', 'F3', 'H3')
+  end
+
+  it 'can not move to spot occupied by one of its same colored pieces' do
+    allow(bk1).to receive(:square_occupied?).with('B3', board).and_return(bk2)
+    bk1.history.push('D4')
+    expect(bk1.possible_moves(board).length).to eql(7)
+    expect(bk1.possible_moves(board)).to_not include('B3')
   end
 end
